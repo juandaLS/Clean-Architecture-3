4f4b12320661078d3c20d08c46eca3ec068ecb26
JuandaLS <juandavid1237890@gmail.com>
2024-10-09 13:48:01 -0500

    Sesion 4 Primera parte

---
+++ NorthWind/NorthWind.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using NorthWind.ConsoleApp.Services;
+using NorthWind.Entities.Interfaces;
+using NorthWind.Writers;
+
+IUserActionWriter Writer = new FileWriter();
+
+AppLogger Logger = new AppLogger(Writer);
+Logger.WriteLog("Application started.");
+
+ProductService Service = new ProductService(Writer);
+Service.Add("Demo", "Azucar refinada");
+
+
+
+
