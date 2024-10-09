0cd284bba6e9542468bfdde834b63924ccaa47c6
JuandaLS <juandavid1237890@gmail.com>
2024-10-09 13:26:40 -0500

    Demo02 Sesion 3 Primera parte

---
+++ Demo02/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using MyLibrary;
+
+Employee Maria = new Secretary();
+Maria.Name = "Maria";
+Employee Pedro = new Manager();
+Pedro.Name = "Pedro";
+Developer Miguel = new Developer();
+Miguel.Name = "Miguel";
+Console.WriteLine($"Dias: {Miguel.GetPaymentDays()}");
+
+WriteEmployeeInfo(Maria);
+WriteEmployeeInfo(Miguel);
+WriteEmployeeInfo(Pedro);
+
+void WriteEmployeeInfo(Employee employee)
+{
+    Console.WriteLine($"{employee.Name}: {employee.CalculateMonthlyPayment()}");
+    Console.WriteLine($"Days: {employee.GetPaymentDays()}");
+}
